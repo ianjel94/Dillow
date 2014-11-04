f3fa34f2c210c5c2ddd5ae8a860f970f365cd519
nimayz <nimayz@vt.edu>
2014-11-04 18:01:42 -0500

    Revert "Revert "Level class and Project started""
    This reverts commit 4f5e981da4564f3cbe6ff1d37456b0bfb063cb6e.

---
+++ src/Level.java
@@ -0,0 +1,12 @@
+
+// -------------------------------------------------------------------------
+/**
+ *  A level contains the user, enemies and other objects
+ *
+ *  @author Ian Jelliffe
+ *  @version Nov 3, 2014
+ */
+public class Level
+{
+
+}
